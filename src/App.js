7ffab7112a17263c755f2f4d2a6b2e547a2e9871
Vivek9744 <priyanshusingh202010@gmail.com>
2023-11-05 11:53:14 +0530

    comiit

--- src/App.js
+++ src/App.js
@@ -5,19 +5,32 @@ import AddMovie from "./components/AddMovie";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; 
 import Detail from "./components/Detail";
 import SearchPage from "./components/SearchPage";
+import { createContext,useEffect,useState } from "react";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+
+const AppState = createContext();
 
 function App() {
+
+  // const[login,setLogin] = useState(false);
+  // const[username,setUsername] = useState('');
   return (
+  //   <AppState.Provider value={{login,setLogin,username,setUsername}}>
     <div className="relative">
-       {/* Wrap your entire app in Router */}
         <Header/>
         <Routes>
           <Route path="/" element={<Cards />} />
           <Route path="/addMovie" element={<AddMovie/>}/>
           <Route path="/detail/:id" element={<Detail/>}/>
           <Route path="/search" element={<SearchPage/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/signup" element={<Signup/>}/>
         </Routes>
+
     </div>
+   //  </AppState.Provider>
   );
 }
 export default App;
+//export {AppState}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import AddIcon from '@mui/icons-material/Add';
 import { Button } from '@mui/material';
 import { Link } from 'react-router-dom';
 import SavedSearchIcon from '@mui/icons-material/SavedSearch';
+//import AppState from '../App.js'
 
 const Header = () => {
+
+ // const useAppstate = useContext(AppState);
+
   return (
     <div className='bg-transparent'>
       <div className='fixed top-0 left-0 w-full z-10'>
@@ -19,6 +23,7 @@ const Header = () => {
               </h1>
             </Button>
           </Link>
+          {/* {useAppstate.login ? */}
           <Link to='/addMovie'>
             <Button>
               <h1 className='text-red-300 cursor-pointer text-xl'>
@@ -26,6 +31,15 @@ const Header = () => {
               </h1>
             </Button>
           </Link>
+          :
+          <Link to='/login'>
+            <Button>
+              <h1 className='text-red-300 cursor-pointer text-xl'>
+                 Login
+              </h1>
+            </Button>
+          </Link>
+          {/* } */}
         </div>
       </div>
     </div>
